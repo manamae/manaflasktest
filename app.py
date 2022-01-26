d9802c9a1b1d9f240601d3ec1ecb7c781915fc61
manamae <87465322+manamae@users.noreply.github.com>
2022-01-26 18:14:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, redirect, request, session,Response
-import mysql.connector
-
+# import mysql.connector
+import psycopg2
 
 
 app = Flask(__name__)
 
-mydb = mysql.connector.connect(
+conn = psycopg2.connect(
 	host = "ec2-54-157-16-125.compute-1.amazonaws.com",
 	user = "znwahnrdcbtjjb",
 	password = "a3b7b147ad6cbc9a3b07b51f02b9c96ca4d510607202f242706f54b255d58b2a",
 	database = "d9mvoqrjshij7o"
 )
+conn.autocommit = True
 
 @app.route("/")
 @app.route("/home")
@@ -21,14 +22,28 @@ def home():
 @app.route("/db", methods = ['POST', 'GET'])
 def db():
 	x = "not connected"
-	if(mydb):
-		x = "connected"
-	return render_template("db.html",msg = x)
-	# if request.method == 'GET':
-	# 	mycursor = mydb.cursor()
-	# 	return render_template("db.html",msg = "x")
-	# if request.method == 'POST':
-	# 	values = request.form.to_dict()
+	result = "nothing"
+	if request.method == 'GET':
+		if(conn):
+			cursor = conn.cursor()
+			# cursor.execute("SELECT * from test")
+			cursor.execute("SELECT * from test")
+			result = cursor.fetchall()
+			x = "connected"
+		return render_template("db.html",c = x,data = result)
+
+	if request.method == 'POST':
+		values = request.form.to_dict()
+		val = values["val"]
+		cursor = conn.cursor()
+		# q = "ALTER TABLE test ADD COLUMN ID SERIAL PRIMARY KEY;;"
+		# q = "ALTER TABLE test DROP COLUMN id;"
+		# cursor.execute(q)
+		q = "INSERT INTO TEST (name) VALUES ('sa')"
+		cursor.execute(q,(val,))
+		conn.commit()
+
+		return redirect("/db")
 	
 
 if __name__ == '__main__':
